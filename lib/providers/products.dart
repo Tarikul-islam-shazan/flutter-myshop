440a1a04754f77acfbae5f076dbfcfd7bb28f31b
Tarikul-islam-shazan <Tarikul@brainstation23.com>
2022-02-27 20:55:52 +0600

    product overview screen favourite option added

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -41,6 +41,10 @@ class Products with ChangeNotifier {
     return [..._items];
   }
 
+  List<Product> get favouriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
+  }
+
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
   }
